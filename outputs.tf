13297587b7def01803bf358ef05c2cf5885f1634
Hugo Bollon <hugo.bollon@camptocamp.com>
2023-04-20 10:25:01 +0200

    feat: export security groups ids to module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,11 @@
 output "alb_dns_name" {
   value = aws_lb.this.dns_name
 }
+
+output "alb_security_group_id" {
+  value = aws_security_group.load_balancer_security_group.id
+}
+
+output "service_security_group_id" {
+  value = aws_security_group.service_security_group.id
 }
